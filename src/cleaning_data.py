aebcffeb81804adc5f30b83c86054d0c20f45ea3
Keon Nartey <elorm10@yahoo.co.uk>
2024-05-02 23:13:36 +0000

    created tests branch

--- src/cleaning_data.py
+++ src/cleaning_data.py
@@ -17,15 +17,21 @@ class Environment:
 
     def average_sulphur_dioxide(self) -> float:
         """Calculating the mean So2 in India."""
+        self.df['so2'].fillna(0, inplace=True)
         return float(self.df["so2"].mean())
 
     def average_nitrogen_oxide(self) -> float:
         """Calculating the man No2 in India."""
+        self.df['no2'].fillna(0, inplace=True)
         return float(self.df["no2"].mean())
 
     def state_max_so2(self) -> Any:
         """State with the maximum So2 production."""
         return self.df.groupby("state")["so2"].max()
+
+    def print_data(self) -> None:
+        """Prints the data."""
+        print(self.df.columns)
 
 
 if __name__ == "__main__":
@@ -34,5 +40,8 @@ if __name__ == "__main__":
         "Environmental-Data-Visualization-/data/data.csv"
     )
     environment = Environment(path)
-    print(environment.average_nitrogen_oxide())
-    print(environment.state_max_so2())
+    environment.print_data()
+    print(environment.df.dtypes)
+
+    # print(environment.average_nitrogen_oxide())
+    # print(environment.state_max_so2())
